62ed6b3dd795f5065d2daab96bab0821afe4b8f5
Dang Thanh Hung <dangthanhhung@kenhtao.site>
2026-03-09 18:02:22 +0700

    Add .gitignore for backend

---
+++ src/config/db.js
@@ -0,0 +1,34 @@
+const Sequelize = require("sequelize");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+);
+
+const connectDB = async () => {
+  try {
+    const res = await sequelize.authenticate();
+    await sequelize.sync({ alert: true });
+    console.log("Connect DB is success!");
+  } catch (e) {
+    console.error("Unable to connect to the database:", e);
+  }
+};
+
+module.exports = { sequelize, connectDB };
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const compression = require("compression");
+const { connectDB } = require("./config/db");
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+//init middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(compression());
+
+//init DB
+connectDB();
+
+//body parser
+app.use(express.json());
+
+//init routes
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+//handle errors
+app.use((req, res, next) => {
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  const statusCode = error.status || 500;
+  return res.status(statusCode).json({
+    status: "Err",
+    code: statusCode,
+    message: error.message || "Internal Server Error",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
