78d8c13039fa767554cb6418715fa1800c056e62
ThHungg <dth052k4@gmail.com>
2026-05-26 23:11:32 +0700

    Fix refreshToken

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -53,10 +53,10 @@ const VerifyOtp = async (req, res) => {
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
-
     return res.status(200).json(newResponse);
   } catch (e) {
     console.log(e);
