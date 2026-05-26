6ff3f9daceed7923f6ccd8d0fb20ea095aa8bb50
ThHungg <dth052k4@gmail.com>
2026-05-26 10:15:22 +0700

    Update

--- src/app/student/page.tsx
+++ src/app/student/page.tsx
@@ -43,6 +43,7 @@ const StudentPage = () => {
     <div className="p-8">
       <StudentInfoCard />
       <h5 className="flex items-center gap-2 font-bold mb-4">
+      Update
         <svg
           xmlns="http://www.w3.org/2000/svg"
           width="1em"
